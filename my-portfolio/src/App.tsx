a22670926de56298e57b1a8bdaf81052e21fdaeb
phoozies <tpvo.personal@gmail.comgit config --global user.name>
2025-09-08 16:41:58 -0500

    feat: remove education section

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -6,7 +6,6 @@ import About from './components/About.tsx'
 import Skills from './components/Skills.tsx'
 import Experience from './components/Experience.tsx'
 import Projects from './components/Projects.tsx'
-import Education from './components/Education.tsx'
 import Contact from './components/Contact.tsx'
 import Footer from './components/Footer.tsx'
 
@@ -22,7 +21,6 @@ function App() {
         <Skills />
         <Experience />
         <Projects />
-        <Education />
         <Contact />
       </main>
       <Footer />
