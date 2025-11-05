6580d1b39ebb005577f3025ac0289e4c2ff52711
phoozies <tpvo.personal@gmail.comgit config --global user.name>
2025-11-05 09:30:26 -0600

    config: url update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/phoozies/' : '/',
+  base: mode === 'production' ? '/my-portfolio/' : '/',
   build: {
     outDir: 'dist'
   }
